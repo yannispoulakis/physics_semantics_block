ea67495b2cf803f0c66051e04cff24b1df13631b
Yannis_Poulakis <giannispoy@gmail.com>
2021-12-30 00:32:39 +0200

    updated with /register_resource

--- flask_main.py
+++ flask_main.py
@@ -5,20 +5,30 @@ from static_params import cluster_properties
 import logging
 import ontospy
 from ontospy.ontodocs.viz.viz_d3dendogram import *
+import errno, os, stat, shutil
+
 
 onto = get_ontology("physics_V0.1.owl").load()
 onto.base_iri
 
 
 def cluster_already_exists(arg):
-    """Deletes the input cluster from the datastore"""
+    """Deletes the input cluster from the datastore if a new one is submitted with the same name"""
     for i in onto.Cluster.instances():
-        if i.name == arg['name']:
-            logging.info(i.name + ' already exists!')
-            destroy_entity(i)
-            r = True
-        else:
-            r = False
+        if type(arg) != str:
+            if i.name == arg['name']:
+                logging.info(i.name + ' already exists!')
+                destroy_entity(i)  # method of owlready2
+                r = True
+            else:
+                r = False
+        elif type(arg) == str:
+            if i.name == arg:
+                logging.info(i.name + ' already exists!')
+                destroy_entity(i)  # method of owlready2
+                r = True
+            else:
+                r = False
     return r
 
 
@@ -28,6 +38,26 @@ app = Flask(__name__)
 @app.route("/")
 def render_home():
     return render_template("main_page.html")
+
+
+@app.route("/register_resource", methods=["GET", "POST"])
+def render_register_resource():
+    if request.method == "POST":
+        cluster_instance = onto.Cluster()
+        print(request.form.get("clusterName"))
+        if cluster_already_exists(request.form.get("clusterName")):
+            logging.info('Updating ' + request.form.get("clusterName"))
+
+        for item in list(onto.data_properties()):
+            if onto.Cluster in item.domain:
+                # check whether the POSTed data have info for all the cluster data properties
+                if item.name in list(x[1] for x in request.form):
+                    setattr(cluster_instance, item.name, request.form.get("clusterName"))
+                    logging.info(cluster_instance.get_name() + ' saved', )
+        return render_template("resource_registered.html")
+
+    elif request.method == "GET":
+        return render_template("test_collapsible.html")
 
 
 @app.route("/add_resource", methods=["POST"])
@@ -75,7 +105,7 @@ def get_available_clusters():
         d[i.name] = {k: v for k, v in d[i.name].items() if k in cluster_properties}
     return json.dumps(d)
 
-import errno, os, stat, shutil
+
 
 def handleRemoveReadonly(func, path, exc):
   excvalue = exc[1]
